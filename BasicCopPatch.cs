bb05bd96392a5ce7c8384af79ff039feab204d69
emildobber <emildobber2@Hotmail.com>
2023-08-22 12:16:25 +0200

    first commit

---
+++ BasicCopPatch.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using Reptile;
+using UnityEngine;
+
+namespace ACABmod
+{
+    [HarmonyPatch(typeof(BasicCop))]
+    [HarmonyPatch(nameof(BasicCop.UpdateState))]
+    public class BasicCopPatch
+    {
+        static bool Prefix(BasicCop __instance)
+        {
+            if (__instance.basicCopState == BasicCop.BasicCopState.HITRESPONSE && __instance.HP <= 0 && 
+                (__instance.hitBoxResponse.State != EnemyHitResponse.HitResponseState.KNOCKBACK ||
+                 __instance.hitBoxResponse.State != EnemyHitResponse.HitResponseState.KNOCKDOWN ||
+                 __instance.hitBoxResponse.State != EnemyHitResponse.HitResponseState.NONE) &&
+                __instance.hitBoxResponse.State == EnemyHitResponse.HitResponseState.LYINGDOWN)
+            {
+                Debug.Log("Cop dead");
+                __instance.SetState(BasicCop.BasicCopState.DIE);
+                return false;
+            }
+            
+            return true;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace ACABmod
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            
+            var harmony = new Harmony("io.mills.acabmod");
+            
+            harmony.PatchAll();
+        }
+    }
+}
