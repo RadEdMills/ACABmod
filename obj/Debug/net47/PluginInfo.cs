b4ab0c70f127b064e5a93965c1472a0476d54213
emildobber <emildobber2@Hotmail.com>
2023-08-22 12:20:21 +0200

    removed gitignore files

--- obj/Debug/net47/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace ACABmod
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "ACABmod";
-        public const string PLUGIN_NAME = "ACABmod";
-        public const string PLUGIN_VERSION = "0.0.1";
-    }
-}
